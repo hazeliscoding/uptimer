c1190de85221989734ec10337b52f346a20d37ae
hazeliscoding <duck_soop@outlook.com>
2024-07-30 10:47:35 -0500

    feat: 🎸 finished setting up apollo server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,74 @@
-import { Express, NextFunction, Request, Response } from 'express';
+import {
+  Express,
+  json,
+  NextFunction,
+  Request,
+  Response,
+  urlencoded,
+} from 'express';
 import http from 'http';
-import { PORT } from './config';
+import cors from 'cors';
+import coookieSession from 'cookie-session';
+
+import { ApolloServer } from '@apollo/server';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { expressMiddleware } from '@apollo/server/express4';
+
+import {
+  CLIENT_URL,
+  NODE_ENV,
+  PORT,
+  SCRERET_KEY_ONE,
+  SCRERET_KEY_TWO,
+} from './config';
+
+const typeDefs = `#graphql
+  type User {
+    username: String
+  }
+
+  type Query {
+    user: User
+  }
+`;
+
+const resolvers = {
+  Query: {
+    user: () => ({ username: 'John Doe' }),
+  },
+};
 
 export default class MonitorServer {
   private app: Express;
   private httpServer: http.Server;
+  private apolloServer: ApolloServer;
 
   constructor(app: Express) {
     this.app = app;
     this.httpServer = new http.Server(app);
+
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+    this.apolloServer = new ApolloServer({
+      schema,
+      introspection: NODE_ENV !== 'production',
+      plugins: [
+        ApolloServerPluginDrainHttpServer({ httpServer: this.httpServer }),
+        NODE_ENV === 'production'
+          ? ApolloServerPluginLandingPageDisabled()
+          : ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+      ],
+    });
   }
 
   async start(): Promise<void> {
+    /**
+     * Note that you must call the start() method on the ApolloServer
+     * instance before passing the instance to expressMiddleware
+     */
+    await this.apolloServer.start();
     this.standardMiddleware(this.app);
     this.startServer();
   }
@@ -22,6 +79,33 @@ export default class MonitorServer {
       res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
       next();
     });
+    app.use(
+      coookieSession({
+        name: 'session',
+        keys: [SCRERET_KEY_ONE, SCRERET_KEY_TWO],
+        maxAge: 24 * 7 * 3600000,
+        secure: NODE_ENV !== 'development',
+        ...(NODE_ENV !== 'development' && { sameSite: 'none' }),
+      })
+    );
+    this.graphqlRoute(app);
+  }
+
+  private graphqlRoute(app: Express): void {
+    app.use(
+      '/graphql',
+      cors({
+        origin: CLIENT_URL,
+        credentials: true,
+      }),
+      json({ limit: '200mb' }),
+      urlencoded({ extended: true, limit: '200mb' }),
+      expressMiddleware(this.apolloServer, {
+        context: async ({ req, res }: { req: Request; res: Response }) => {
+          return { req, res };
+        },
+      })
+    );
   }
 
   private async startServer(): Promise<void> {
