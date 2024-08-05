8d9cf458737dd08dd2c3adc71a940b465ac3a08b
hazeliscoding <duck_soop@outlook.com>
2024-08-04 20:46:00 -0500

    feat: 🎸 monitor create and get service methods

---
+++ src/services/monitor.service.ts
@@ -0,0 +1,113 @@
+import { Model, Op } from 'sequelize';
+import { IMonitorDocument } from '../interfaces/monitor.interface';
+import { MonitorModel } from '../models/monitor.model';
+import { getSingleNotificationGroup } from './notification.service';
+
+/**
+ * Create a new monitor
+ * @param data
+ * @returns {Promise<IMonitorDocument>}
+ */
+export const createMonitor = async (
+  data: IMonitorDocument
+): Promise<IMonitorDocument> => {
+  try {
+    const result: Model = await MonitorModel.create(data);
+    return result.dataValues;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+/**
+ * Get either all monitors (active or inactive) or just active for a user
+ * @param userId
+ * @param active
+ * @returns
+ */
+export const getUserMonitors = async (
+  userId: number,
+  active?: boolean
+): Promise<IMonitorDocument[]> => {
+  try {
+    const monitors: IMonitorDocument[] = (await MonitorModel.findAll({
+      raw: true,
+      where: {
+        [Op.and]: [
+          {
+            userId,
+            ...(active && {
+              active: true,
+            }),
+          },
+        ],
+      },
+      order: [['createdAt', 'DESC']],
+    })) as unknown as IMonitorDocument[];
+    return monitors;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+/**
+ * Get active monitors for all users
+ * @returns {Promise<IMonitorDocument[]>}
+ */
+export const getAllUsersActiveMonitors = async (): Promise<
+  IMonitorDocument[]
+> => {
+  try {
+    const monitors: IMonitorDocument[] = (await MonitorModel.findAll({
+      raw: true,
+      where: { active: true },
+      order: [['createdAt', 'DESC']],
+    })) as unknown as IMonitorDocument[];
+    return monitors;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+/**
+ * Get all active monitors for a user
+ * @param userId
+ * @returns {Promise<IMonitorDocument[]>}
+ */
+export const getUserActiveMonitors = async (
+  userId: number
+): Promise<IMonitorDocument[]> => {
+  try {
+    const monitors: IMonitorDocument[] = await getUserMonitors(userId, true);
+    for (const monitor of monitors) {
+      console.log(monitor);
+    }
+    return monitors;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+/**
+ * Get monitor by id
+ * @param monitorId
+ * @returns {Promise<IMonitorDocument>}
+ */
+export const getMonitorById = async (
+  monitorId: number
+): Promise<IMonitorDocument> => {
+  try {
+    const monitor: IMonitorDocument = (await MonitorModel.findOne({
+      raw: true,
+      where: { id: monitorId },
+    })) as unknown as IMonitorDocument;
+    let updatedMonitor: IMonitorDocument = { ...monitor };
+    const notifications = await getSingleNotificationGroup(
+      updatedMonitor.notificationId!
+    );
+    updatedMonitor = { ...updatedMonitor, notifications };
+    return updatedMonitor;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
