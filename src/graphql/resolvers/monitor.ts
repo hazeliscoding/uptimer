491b18e84c562d7346c1c8b7e59a21960e00eadb
hazeliscoding <duck_soop@outlook.com>
2024-08-05 14:24:11 -0500

    feat: 🎸 toggle monitor and mutation resolver

--- src/graphql/resolvers/monitor.ts
+++ src/graphql/resolvers/monitor.ts
@@ -4,8 +4,9 @@ import {
   IMonitorDocument,
 } from '../../interfaces/monitor.interface';
 import logger from '../../server/logger';
-import { createMonitor } from '../../services/monitor.service';
-import { authenticateGraphQLRoute } from '../../utils/utils';
+import { createMonitor, toggleMonitor } from '../../services/monitor.service';
+import { startSingleJob, stopSingleBackgroundJob } from '../../utils/jobs';
+import { appTimeZone, authenticateGraphQLRoute } from '../../utils/utils';
 
 export const monitorResolver = {
   Mutation: {
@@ -21,10 +22,41 @@ export const monitorResolver = {
       if (body.active && monitor?.active) {
         // TODO: Start created monitor
         logger.info(`Monitor ${monitor.id} started`);
+        startSingleJob(body.name, appTimeZone, 10, () => {
+          logger.info('This is callled every 10 seconds');
+        });
       }
 
       return {
         monitors: [monitor],
+      };
+    },
+    async toggleMonitor(
+      _: undefined,
+      args: IMonitorArgs,
+      contextValue: AppContext
+    ) {
+      const { req } = contextValue;
+      authenticateGraphQLRoute(req);
+      const { monitorId, userId, name, active } = args.monitor!;
+      const results: IMonitorDocument[] = await toggleMonitor(
+        monitorId!,
+        userId,
+        active as boolean
+      );
+      if (!active) {
+        logger;
+        stopSingleBackgroundJob(name, monitorId!);
+      } else {
+        // TODO: Add a resume method here
+        logger.info(`Resume monitor`);
+        startSingleJob(name, appTimeZone, 10, () => {
+          logger.info('Resumed after 10 seconds');
+        });
+      }
+
+      return {
+        monitors: results,
       };
     },
   },
